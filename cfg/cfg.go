ec628c6a4851a1206b672a15571e12b8fb10c806
Vatican Embassy <vaticanembassy@porcod.io>
2019-09-08 14:23:51 +0200

    fetch pastebin data

---
+++ cfg/cfg.go
@@ -0,0 +1,27 @@
+package cfg
+
+import (
+	"flag"
+)
+
+var INFORGE_FORUM = "https://www.inforge.net/forum/forums/passwords-dump.1565/index.rss"
+var PASTEBIN_LINK = "pastebin.com/(?P<path>[a-zA-Z0-9_/-]+)"
+var PASTEBIN_URL = "https://pastebin.com/raw/%s"
+
+type Config struct {
+	Feed string
+	Outdir string
+	Dry bool
+}
+
+func ReadArgs() (*Config) {
+	c := Config{}
+	flag.StringVar(&c.Feed, "feed", INFORGE_FORUM,
+			"URL of RSS or Atom feed to parse")
+	flag.StringVar(&c.Outdir, "output-dir", "dumps",
+			"Directory used to store the fetched files")
+	flag.BoolVar(&c.Dry, "dry-run", false,
+			"do not fetch files, just print the URLs")
+	flag.Parse()
+	return &c
+}
--- main.go
+++ main.go
@@ -1,27 +1,92 @@
 package main
 
 import (
+	"os"
 	"fmt"
+	"path"
 	"regexp"
+	"strings"
+	"net/http"
+	"io/ioutil"
+	"github.com/VaticanEmbassy/inforcefeed/cfg"
 	"github.com/mmcdole/gofeed"
 )
 
-var INFORGE_FORUM = "https://www.inforge.net/forum/forums/passwords-dump.1565/index.rss"
-var PASTEBIN_LINK = "src=\"//pastebin.com/embed_js/(.+)/noheader\""
-var PASTEBIN_URL = "https://pastebin.com/embed_js/%s/noheader"
+
+type Fetcher struct {
+	config *cfg.Config
+}
+
+
+func (f *Fetcher) fetchPaste(id string) bool {
+	resp, err := http.Get(fmt.Sprintf(cfg.PASTEBIN_URL, id))
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return false
+	}
+	if _, err := os.Stat(f.config.Outdir); os.IsNotExist(err) {
+		os.MkdirAll(f.config.Outdir, 0755)
+	}
+	destFile := path.Join(f.config.Outdir, id)
+	err = ioutil.WriteFile(destFile, body, 0644)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+
+func (f *Fetcher) Run() {
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL(f.config.Feed)
+	reLink := regexp.MustCompile(cfg.PASTEBIN_LINK)
+	groupNames := reLink.SubexpNames()
+	seen := map[string]bool{}
+	for _, item := range feed.Items {
+		for _, match := range reLink.FindAllStringSubmatch(item.Content, -1) {
+			for groupIdx, group := range match {
+				name := groupNames[groupIdx]
+				if name != "path" {
+					continue
+				}
+				var id string
+				pathPieces := strings.Split(group, "/")
+				id = pathPieces[0]
+				if len(pathPieces) > 1 && pathPieces[1] != "" {
+					id = pathPieces[1]
+				}
+				if id == "" {
+					continue
+				}
+				if _, ok := seen[id]; ok {
+					continue
+				}
+				url := fmt.Sprintf(cfg.PASTEBIN_URL, id)
+				fmt.Printf("%s\n", url)
+				seen[id] = true
+				if !f.config.Dry {
+					f.fetchPaste(id)
+				}
+			}
+		}
+	}
+}
+
+
+func NewFetcher(config *cfg.Config) *Fetcher {
+	f := new(Fetcher)
+	f.config = config
+	return f
+}
 
 
 func main() {
-	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL(INFORGE_FORUM)
-	fmt.Println(feed.Title)
-	reLink := regexp.MustCompile(PASTEBIN_LINK)
-	for _, item := range feed.Items {
-		//fmt.Println(item.GUID)
-		link := reLink.FindAllStringSubmatch(item.Content, -1)
-		if link == nil {
-			continue
-		}
-		fmt.Println(fmt.Sprintf(PASTEBIN_URL, link[0][1]))
-	}
+	config := cfg.ReadArgs()
+
+	fetcher := NewFetcher(config)
+	fetcher.Run()
 }
