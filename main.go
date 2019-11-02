b0bea759bbb12341e0b49125acf90c011e9b10dc
Vatican Embassy <vaticanembassy@porcod.io>
2019-11-02 17:47:18 +0100

    fix #1 store last seen GUID

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"fmt"
+	"log"
 	"path"
 	"regexp"
 	"strings"
@@ -21,11 +22,13 @@ type Fetcher struct {
 func (f *Fetcher) fetchPaste(id string) bool {
 	resp, err := http.Get(fmt.Sprintf(cfg.PASTEBIN_URL, id))
 	if err != nil {
+		log.Println(err)
 		return false
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
+		log.Println(err)
 		return false
 	}
 	if _, err := os.Stat(f.config.Outdir); os.IsNotExist(err) {
@@ -34,19 +37,61 @@ func (f *Fetcher) fetchPaste(id string) bool {
 	destFile := path.Join(f.config.Outdir, id)
 	err = ioutil.WriteFile(destFile, body, 0644)
 	if err != nil {
+		log.Println(err)
 		return false
 	}
 	return true
 }
 
 
-func (f *Fetcher) Run() {
+func (f *Fetcher) writeLastSeen(guid string) bool {
+	if _, err := os.Stat(f.config.Outdir); os.IsNotExist(err) {
+		os.MkdirAll(f.config.Outdir, 0755)
+	}
+	lsFile := path.Join(f.config.Outdir, ".last_guid")
+	err := ioutil.WriteFile(lsFile, []byte(guid), 0644)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+
+func (f *Fetcher) readLastSeen() string {
+	lsFile := path.Join(f.config.Outdir, ".last_guid")
+	if _, err := os.Stat(lsFile); os.IsNotExist(err) {
+		return ""
+	}
+	file, err := os.Open(lsFile)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	defer file.Close()
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	return strings.TrimSpace(string(b))
+}
+
+
+func (f *Fetcher) Run() int {
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL(f.config.Feed)
 	reLink := regexp.MustCompile(cfg.PASTEBIN_LINK)
 	groupNames := reLink.SubexpNames()
 	seen := map[string]bool{}
-	for _, item := range feed.Items {
+	count := 0
+	for idx, item := range feed.Items {
+		lastSeenGUID := f.readLastSeen()
+		if lastSeenGUID != "" && item.GUID == lastSeenGUID {
+			break
+		}
+		if idx == 0 && !f.config.Dry && item.GUID != "" {
+			f.writeLastSeen(item.GUID)
+		}
 		for _, match := range reLink.FindAllStringSubmatch(item.Content, -1) {
 			for groupIdx, group := range match {
 				name := groupNames[groupIdx]
@@ -71,9 +116,11 @@ func (f *Fetcher) Run() {
 				if !f.config.Dry {
 					f.fetchPaste(id)
 				}
+				count += 1
 			}
 		}
 	}
+	return count
 }
 
 
